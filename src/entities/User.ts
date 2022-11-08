4d18d2c95f6e591ce4af6346e2576cd474bd1e7f
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-11-08 15:03:34 -0300

    creating repositories

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,13 +1,37 @@
-import { Entity } from "typeorm";
+import {
+    Entity,
+    PrimaryColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn
+} from "typeorm";
+import { v4 as uuid } from 'uuid';
 
 @Entity("users")
 class User {
-    id: string;
+    @PrimaryColumn()
+    readonly id: string;
+
+    @Column()
     name: string;
+
+    @Column()
     email: string;
+
+    @Column()
     admin: boolean;
+
+    @CreateDateColumn()
     created_at: Date;
+
+    @UpdateDateColumn()
     updated_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+    }
 }
 
 export { User }; 
