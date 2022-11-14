8f8df8843d9ff776513aaf41bb6b38febbb911f6
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-11-14 20:17:46 -0300

    building controllers

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,3 +1,5 @@
+import { getCustomRepository } from 'typeorm';
+
 import { UsersRepositories } from '../repositories/UsersRepositories';
 
 interface IUserRequest {
@@ -8,7 +10,7 @@ interface IUserRequest {
 
 class CreateUserService {
     async execute({ name, email, admin }: IUserRequest) {
-        const usersRepository = new UsersRepositories();
+        const usersRepository = getCustomRepository(UsersRepositories);
 
         if (!email) {
             throw new Error('Email incorrect');
