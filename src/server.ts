0424c3a82475bd08b66df3ca187a45720cf75012
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-11-16 13:09:24 -0300

    handling errors in the application

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
 import { router } from './routes';
 
@@ -7,8 +7,24 @@ import "./database";
 
 const app = express();
 
+//middleware que habilita o json na requisição
 app.use(express.json());
 
+//middleware entre server e router e o router chama os controllers
 app.use(router);
+
+//middleware que verifica e faz uma tratativa nos erros
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal Server Error'
+    })
+})
 
 app.listen(3000, () => console.log('Server is running NLW'));
